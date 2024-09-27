0d7de43145c571bd218c52633f5d98d563c3810c
bluedinosaur139 <138134727+bluedinosaur139@users.noreply.github.com>
2024-09-27 17:36:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path');
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, 'resources/CatGPTIcon.png'), // Update the icon path to resources folder
     webPreferences: {
       nodeIntegration: true
     }
