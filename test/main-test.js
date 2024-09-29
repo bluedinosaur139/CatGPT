5a209b5ed54e0261e9903bff2e98ed13388e995f
bluedinosaur139 <138134727+bluedinosaur139@users.noreply.github.com>
2024-09-29 06:51:40 -0400

    Update main-test.js
    now in js and not json format lmao silly ai

--- test/main-test.js
+++ test/main-test.js
@@ -1,19 +1,7 @@
-{
-  "name": "catgpt-electron",
-  "version": "1.0.0",
-  "main": "main.js",
-  "scripts": {
-    "start": "electron .",
-    "build": "electron-packager . CatGPT --platform=linux --arch=arm64 --arch=x64 --overwrite --icon=./CatGPTIcon.png --extra-resource=./resources",
-    "test": "mocha 'test/**/*.js'"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "description": "",
-  "devDependencies": {
-    "electron": "^32.1.2",
-    "electron-packager": "^17.1.2",
-    "mocha": "^10.0.0"
-  }
-}
+const assert = require('assert');
+
+describe('Basic Mocha Test', function() {
+    it('should return true', function() {
+        assert.equal(true, true);
+    });
+});
