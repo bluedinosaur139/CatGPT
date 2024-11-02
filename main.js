3e5d9e7e04c8842c0ea4f9414c0707e085a6da0d
bluedinosaur139 <138134727+bluedinosaur139@users.noreply.github.com>
2024-11-02 12:08:39 -0400

    Update main.js
    attempt 2 to add transparancy

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
-
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     icon: path.join(__dirname, 'resources/CatGPTIcon.png'), // Update the icon path to resources folder
+    transparent: true, // Enable window transparency
+    frame: false, // Remove window frame for a seamless look
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
-  win.loadURL('https://chat.openai.com/')
+  win.loadURL('https://chat.openai.com/');
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
